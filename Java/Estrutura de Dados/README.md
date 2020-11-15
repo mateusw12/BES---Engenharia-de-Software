b3f881497b3292aee3a0694d6b2215d203ab10a6
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2020-11-15 19:23:50 -0300

    Add files via upload

---
+++ Java/Estrutura de Dados/README.md
@@ -0,0 +1,6 @@
+# Arranjos 
+
+Projeto Java para os exercícios sobre Arranjos.
+
+
+[CorrecaoArranjos.java](src/br/univille/estd/arranjos/CorrecaoArranjos.java) - Arquivo com a solução desenvolvida.
---
+++ Java/Estrutura de Dados/src/br/univille/estd/arranjos/Arranjos.java
@@ -0,0 +1,101 @@
+package br.univille.estd.arranjos;
+
+
+/**
+ * Lista de Exercícios
+ * Alterem este arquivo
+ */
+public class Arranjos {
+	
+	/**
+	 * Retorne o valor 0
+	 * @return
+	 */
+	public static int exercicio00() {
+		return 0;
+	}
+	
+	/**
+	 * Crie um arranjo de inteiros de 3 posicoes, com valores zeros
+	 * e retorne por parametro
+	 * @return
+	 */
+	public static int[] exercicio01() {
+		return null;
+	}
+	
+	/**
+	 * Crie um arranjo de Strings de 3 posicoes com os valores "aa","bb" e "cc"
+	 * e retorne por parametro
+	 */
+	public static String[] exercicio02() {
+		return null;
+	}
+	
+	/**
+	 * Retorne o tamanho do vetor
+	 */
+	public static int exercicio03(int[] vetor) {
+		return -1;
+	}
+	
+	/**
+	 * Retorne o elemento da posicao 4
+	 */
+	public static int exercicio04(int[] vetor) {
+		return 0;
+	}
+	
+	/**
+	 * Modifique todas as posicoes do vetor para o valor -1
+	 * Retorne o vetor modificado
+	 */
+	public static int[] exercicio05(int[] vetor) {
+		return null;
+	}
+	
+	/**
+	 * Some os valores contidos no arranjo e retorne o total
+	 */
+	public static int exercicio06(int[] vetor) {
+		return -1;
+	}
+	
+	/**
+	 * Conte os numeros impares contidos no arranjo e retorne o total
+	 */
+	public static int exercicio07(int[] vetor) {
+		return -1;
+	}
+	
+	/**
+	 * Copie os valores do vetor A para o Vetor B. Retorne o vetor modificado B
+	 * Os vetores A e B sempre terao o mesmo tamanho
+	 */
+	public static int[] exercicio08(int[] vetorA, int[] vetorB) {
+		
+		return vetorA;
+	}
+	
+	/**
+	 * Retorne um vetor com os elementos na ordem inversa do vetor recebido por parametro
+	 * Exemplo: Vetor recebido  3, 5, 6
+	 * Retorno: Vetor retornado 6, 5, 3
+	 */
+	public static int[] exercicio09(int[] vetor) {
+		return null;
+	}
+	
+	/**
+	 * Pesquise no vetor recebido por parametro, a posicao do elemento recebido por parametro.
+	 * Retorne a posicao.
+	 * Exemplo: Vetor recebido  3, 5, 6
+	 *          Elemento 5
+	 * Retorno: A posicao do elemento 5 é 1
+	 * Se o elemento nao existir no vetor retorne -1
+	 */
+	public static int exercicio10(int[] vetor, int elemento) {
+		return -1;
+	}
+
+}
