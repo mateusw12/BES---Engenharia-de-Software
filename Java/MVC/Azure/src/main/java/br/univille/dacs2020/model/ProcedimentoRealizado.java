8b2b07adbcafa5fff7252c57b87721ac6ec9bc75
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2020-11-15 19:31:12 -0300

    Add files via upload

---
+++ Java/MVC/Azure/src/main/java/br/univille/dacs2020/model/ProcedimentoRealizado.java
@@ -0,0 +1,44 @@
+package br.univille.dacs2020.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class ProcedimentoRealizado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String descricao;
+    private String valor;
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.REFRESH})
+    private Procedimento procedimento;
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+    
+}
